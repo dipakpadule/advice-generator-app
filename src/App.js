612eac877b98574fb995667478a875ffc621e783
dipak <sarkar3115@gmail.com>
2023-04-13 17:05:22 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { useEffect } from "react";
 
 function App() {
 
-  const [advice, setAdvice] = useState("");
+  const [advice, setAdvice] = useState("Its easy to situp and take notice, What's difficult taking up and taking action");
+  const [count ,setCount] = useState("117");
 
   const fetchAPIData = () =>{
     fetch("https://api.adviceslip.com/advice")
@@ -14,9 +15,12 @@ function App() {
     })
     .then(data => {
       setAdvice(data.slip.advice);
+      setCount(data.slip.id);
     })
     // console.log(advice);
     document.querySelector("h1").innerText = advice;
+   
+    document.querySelector("span").innerText = count;
   };
 
   useEffect(()=>{
@@ -25,8 +29,8 @@ function App() {
 
   return (
     <article className='advice-gen'>
-      <p>Advice #<span>72</span></p> 
-      <h1>"aking action."</h1>
+      <p>Advice #<span>117</span></p> 
+      <h1> </h1>
       <section className="advice-divider">
 
       </section>
